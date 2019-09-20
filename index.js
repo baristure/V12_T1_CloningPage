532c84a994b2ead983110f6eaa9df4f9b69cde8a
baristure <baristure@hotmail.com>
2019-09-21 01:04:35 +0300

    html-css-js updates
    HTML> favicon fixed
    CSS > small screen phones navbar sizes fixed
    JS > Cross browser smooth effect added

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function scrollFunction() {
   } else {
     //Hide backtotopButton
     backToTopButton.style.display = "none";
+    backToTopButton.classList.add("btnExit");
   }
 }
 
@@ -20,3 +21,90 @@ backToTopButton.addEventListener("click", backtoTop);
 function backtoTop() {
   window.scrollTo(0, 0);
 }
+
+//     NavBar smooth effect
+
+const list = document.getElementsByClassName("main-nav")[0];
+
+// easing functions from http://goo.gl/5HLl8
+Math.easeInOutQuad = function(t, b, c, d) {
+  t /= d / 2;
+  if (t < 1) {
+    return (c / 2) * t * t + b;
+  }
+  t--;
+  return (-c / 2) * (t * (t - 2) - 1) + b;
+};
+
+Math.easeInCubic = function(t, b, c, d) {
+  var tc = (t /= d) * t * t;
+  return b + c * tc;
+};
+
+Math.inOutQuintic = function(t, b, c, d) {
+  var ts = (t /= d) * t,
+    tc = ts * t;
+  return b + c * (6 * tc * ts + -15 * ts * ts + 10 * tc);
+};
+
+// requestAnimationFrame for Smart Animating from http://goo.gl/sx5sts
+var requestAnimFrame = (function() {
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function(callback) {
+      window.setTimeout(callback, 1000 / 60);
+    }
+  );
+})();
+
+function scrollTo(to, callback, duration) {
+  // because it's so fucking difficult to detect the scrolling element, just move them all
+  function move(amount) {
+    document.documentElement.scrollTop = amount;
+    document.body.parentNode.scrollTop = amount;
+    document.body.scrollTop = amount;
+  }
+
+  function position() {
+    return (
+      document.documentElement.scrollTop ||
+      document.body.parentNode.scrollTop ||
+      document.body.scrollTop
+    );
+  }
+  var start = position(),
+    change = to - start,
+    currentTime = 0,
+    increment = 20;
+  duration = typeof duration === "undefined" ? 500 : duration;
+  var animateScroll = function() {
+    // increment the time
+    currentTime += increment;
+    // find the value with the quadratic in-out easing function
+    var val = Math.easeInOutQuad(currentTime, start, change, duration);
+    // move the document.body
+    move(val);
+    // do the animation unless its over
+    if (currentTime < duration) {
+      requestAnimFrame(animateScroll);
+    } else {
+      if (callback && typeof callback === "function") {
+        // the animation is done so lets callback
+        callback();
+      }
+    }
+  };
+  animateScroll();
+}
+
+list.addEventListener("click", e => {
+  const { target } = e;
+  const to = target.getAttribute("data-target");
+  const element = document.getElementById(to);
+  const bodyRect = document.body.getBoundingClientRect();
+  const elemRect = element.getBoundingClientRect();
+  const offset = elemRect.top - bodyRect.top;
+  scrollTo(offset, null, 300);
+});

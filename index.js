da3868be617c0f72b86175e22c0dec833836a197
baristure <baristure@hotmail.com>
2019-09-22 12:09:08 +0300

    index.js update

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
-const backToTopButton = document.querySelector(".back-to-top");
-
-window.addEventListener("scroll", scrollFunction);
-
-function scrollFunction() {
-  /* */
-  if (window.pageYOffset > 535) {
-    //Show bactotopButton
-    backToTopButton.style.display = "block";
-    backToTopButton.classList.remove("btnExit");
-    backToTopButton.classList.add("btnEntrance");
-  } else {
-    //Hide backtotopButton
-    backToTopButton.style.display = "none";
-    backToTopButton.classList.add("btnExit");
-  }
-}
-
-backToTopButton.addEventListener("click", backtoTop);
-
-function backtoTop() {
-  window.scrollTo(0, 0);
-}
-
-//     NavBar smooth effect
-
-const list = document.getElementsByClassName("main-nav")[0];
-
 // easing functions from http://goo.gl/5HLl8
+/*
+t: current time/currnet step
+b: starting position
+c: amount of change (end - start)
+d: total animation time/steps
+*/
+
 Math.easeInOutQuad = function(t, b, c, d) {
+  t /= d / 2;
+  if (t < 1) {
+    return (c / 2) * t * t + b;
+  }
+  t--;
+  return (-c / 2) * (t * (t - 2) - 1) + b;
+};
+function easeInOutQuad(t, b, c, d) {
   t /= d / 2;
   if (t < 1) {
     return (c / 2) * t * t + b;
@@ -41,11 +28,77 @@ Math.easeInCubic = function(t, b, c, d) {
   return b + c * tc;
 };
 
+function easeInOutCubic(t, b, c, d) {
+  t /= d / 2;
+  if (t < 1) return (c / 2) * t * t * t + b;
+  t -= 2;
+  return (c / 2) * (t * t * t + 2) + b;
+}
+
 Math.inOutQuintic = function(t, b, c, d) {
   var ts = (t /= d) * t,
     tc = ts * t;
   return b + c * (6 * tc * ts + -15 * ts * ts + 10 * tc);
 };
+
+//     Back to Top Button Smooth Effect
+
+const backToTopButton = document.querySelector(".back-to-top");
+
+window.addEventListener("scroll", scrollFunction);
+
+// Back to Top Button Show & Hide Function
+
+function scrollFunction() {
+  if (window.pageYOffset > 300) {
+    // Show backToTopButton
+    if (!backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnExit");
+      backToTopButton.classList.add("btnEntrance");
+      backToTopButton.style.display = "block";
+    }
+  } else {
+    // Hide backToTopButton
+    if (backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnEntrance");
+      backToTopButton.classList.add("btnExit");
+      setTimeout(function() {
+        backToTopButton.style.display = "none";
+      }, 250);
+    }
+  }
+}
+
+backToTopButton.addEventListener("click", smoothScrollBackToTop);
+
+// function backToTop() {
+//   window.scrollTo(0, 0);
+// }
+
+//     Back to Top Button Smooth Effect fo Cross Browser
+function smoothScrollBackToTop() {
+  const targetPosition = 0;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  const duration = 750;
+  let start = null;
+
+  window.requestAnimationFrame(step);
+
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    window.scrollTo(
+      0,
+      easeInOutCubic(progress, startPosition, distance, duration)
+    );
+    if (progress < duration) window.requestAnimationFrame(step);
+  }
+}
+
+//     NavBar Smooth effect
+
+const list = document.getElementsByClassName("main-nav")[0];
 
 // requestAnimationFrame for Smart Animating from http://goo.gl/sx5sts
 var requestAnimFrame = (function() {
